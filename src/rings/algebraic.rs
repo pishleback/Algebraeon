485901ba782b3fa832f4d35ebf3be6bf8b5e8c5b
Michael <michael@otopia.co.uk>
2023-08-06 17:13:50 +0100

    Update algebraic.rs

--- src/rings/algebraic.rs
+++ src/rings/algebraic.rs
@@ -1,42 +1,267 @@
 #![allow(dead_code)]
 
 use malachite_nz::integer::Integer;
+use malachite_q::Rational;
 
-use super::nzq::*;
+use super::multipoly::*;
+// use super::nzq::*;
 use super::poly::*;
 use super::ring::ComRing;
 use super::ring::*;
 
-fn root_sum_poly(p: Polynomial<Integer>, q: Polynomial<Integer>) {
-    // x, z = sympy.symbols("x, z")
-    // p = poly_to_sympy(p, x)
-    // q = poly_to_sympy(q, x)
-    // r = sympy.Poly(q.compose(sympy.Poly(z - x)), x, domain = "ZZ[z]")
-    // sum_poly = sympy.Poly(sympy.resultant(p, r)).sqf_part()
-    // return sympy_to_poly(sum_poly, z)
-    todo!();
+pub const QQ_BAR: RationalAlgebraicField = RationalAlgebraicField {};
+
+fn root_sum_poly(p: &Polynomial<Integer>, q: &Polynomial<Integer>) -> Polynomial<Integer> {
+    let zz_poly_of_multi_poly = PolynomialRing::new(&ZZ_MULTIPOLY);
+
+    let x = Variable::new(String::from("x"));
+    let z = Variable::new(String::from("z"));
+
+    let p = p.apply_map(|c| ZZ_MULTIPOLY.constant(c.clone()));
+    let q = q.apply_map(|c| ZZ_MULTIPOLY.constant(c.clone()));
+    let r = ZZ_MULTIPOLY.expand(
+        &zz_poly_of_multi_poly.evaluate(
+            &q,
+            &ZZ_MULTIPOLY.add(
+                ZZ_MULTIPOLY.var(z.clone()),
+                ZZ_MULTIPOLY.neg(ZZ_MULTIPOLY.var(x.clone())),
+            ),
+        ),
+        &x,
+    );
+
+    let root_sum_poly = ZZ_MULTIPOLY
+        .expand(&zz_poly_of_multi_poly.resultant(p, r), &z)
+        .apply_map(|c| ZZ_MULTIPOLY.as_constant(c).unwrap());
+    if root_sum_poly == ZZ_POLY.zero() {
+        root_sum_poly
+    } else {
+        ZZ_POLY.primitive_squarefree_part(root_sum_poly).unwrap()
+    }
 }
 
-fn root_prod_poly(p: Polynomial<Integer>, q: Polynomial<Integer>) {
-    // x, t = sympy.symbols("x, t")
-    // p = poly_to_sympy(p, t)
-    // q = poly_to_sympy(q, x)
-    // r = sympy.Poly(q.homogenize(t), t, domain = "ZZ[x]")
+fn root_prod_poly(p: &Polynomial<Integer>, q: &Polynomial<Integer>) -> Polynomial<Integer> {
+    let zz_poly_of_multi_poly = PolynomialRing::new(&ZZ_MULTIPOLY);
+
+    let x = Variable::new(String::from("x"));
+    let t = Variable::new(String::from("t"));
+
+    let p = p.apply_map(|c| ZZ_MULTIPOLY.constant(c.clone()));
+    let q = zz_poly_of_multi_poly.evaluate(
+        &q.apply_map(|c| ZZ_MULTIPOLY.constant(c.clone())),
+        &ZZ_MULTIPOLY.var(x.clone()),
+    );
+    let r = ZZ_MULTIPOLY.expand(&ZZ_MULTIPOLY.homogenize(&q, &t), &t);
     // #x ** q.degree() * q(t * x ** -1)
-    // prod_poly = sympy.Poly(sympy.resultant(p, r), x, domain = "ZZ").sqf_part()
-    // return sympy_to_poly(prod_poly, x)
-    todo!();
+
+    let root_prod_poly = ZZ_MULTIPOLY
+        .expand(&zz_poly_of_multi_poly.resultant(p, r), &x)
+        .apply_map(|c| ZZ_MULTIPOLY.as_constant(c).unwrap());
+    if root_prod_poly == ZZ_POLY.zero() {
+        root_prod_poly
+    } else {
+        ZZ_POLY.primitive_squarefree_part(root_prod_poly).unwrap()
+    }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RealAlgebraic {
+    a: Rational,               //lower bound
+    b: Rational,               //upper bound
+    poly: Polynomial<Integer>, //a primitive irreducible polynomial of degree >= 2 with a unique real root between a and b
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ComplexAlgebraic {
+    a: Rational,               //lower bound for the real part
+    b: Rational,               //upper bound for the real part
+    c: Rational,               //lower bound for the imaginary part
+    d: Rational,               //upper bound for the imaginary part
+    poly: Polynomial<Integer>, //a primitive irreducible polynomial of degree >= 2 with a unique non-real complex root in the box defined by (a, b, c, d)
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Algebraic {
+    Rational(Rational),
+    Real(RealAlgebraic),
+    Complex(ComplexAlgebraic),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RationalAlgebraicField {}
+
+impl ComRing for RationalAlgebraicField {
+    type ElemT = Algebraic;
+
+    fn to_string(&self, elem: &Self::ElemT) -> String {
+        match elem {
+            Algebraic::Rational(a) => a.to_string(),
+            Algebraic::Real(a) => todo!(),
+            Algebraic::Complex(a) => todo!(),
+        }
+    }
+
+    fn zero(&self) -> Self::ElemT {
+        Algebraic::Rational(Rational::from(0))
+    }
+
+    fn one(&self) -> Self::ElemT {
+        Algebraic::Rational(Rational::from(1))
+    }
+
+    fn neg_mut(&self, elem: &mut Self::ElemT) {
+        todo!()
+    }
+
+    fn add_mut(&self, elem: &mut Self::ElemT, offset: &Self::ElemT) {
+        todo!()
+    }
+
+    fn mul_mut(&self, elem: &mut Self::ElemT, mul: &Self::ElemT) {
+        todo!()
+    }
+
+    fn div(&self, a: Self::ElemT, b: Self::ElemT) -> Result<Self::ElemT, RingDivisionError> {
+        todo!()
+    }
+}
+
+impl IntegralDomain for RationalAlgebraicField {}
+
+impl Field for RationalAlgebraicField {}
 
 #[cfg(test)]
 mod tests {
-    //#[test]
+    use super::super::poly::*;
+    use super::*;
+
+    #[test]
     fn test_root_sum_poly() {
-        todo!();
+        for (f, g, exp) in vec![
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-3), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-5), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-8), Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-7), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(2)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(3)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-5), Integer::from(6)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(-2), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-2), Integer::from(0), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![
+                    Integer::from(-7),
+                    Integer::from(5),
+                    Integer::from(3),
+                    Integer::from(-1),
+                ]),
+            ),
+        ] {
+            println!();
+            let rsp = root_sum_poly(&f, &g);
+            println!("f = {}", ZZ_POLY.to_string(&f));
+            println!("g = {}", ZZ_POLY.to_string(&g));
+            println!(
+                "exp = {}    exp_factored = {:?}",
+                ZZ_POLY.to_string(&exp),
+                ZZ_POLY.factorize_by_kroneckers_method(&exp)
+            );
+            println!(
+                "rsp = {}    rsp_factored = {:?}",
+                ZZ_POLY.to_string(&rsp),
+                ZZ_POLY.factorize_by_kroneckers_method(&rsp)
+            );
+            assert!(ZZ_POLY.are_associate(&exp, &rsp));
+        }
     }
 
-    //#[test]
+    #[test]
     fn test_root_prod_poly() {
-        todo!();
+        for (f, g, exp) in vec![
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(0)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-3), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-5), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-15), Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-7), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(1)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(2)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(3)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(6)]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-1), Integer::from(-2), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-2), Integer::from(0), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![
+                    Integer::from(4),
+                    Integer::from(0),
+                    Integer::from(-12),
+                    Integer::from(0),
+                    Integer::from(1),
+                ]),
+            ),
+            (
+                ZZ_POLY.from_coeffs(vec![Integer::from(-2), Integer::from(0), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-2), Integer::from(0), Integer::from(1)]),
+                ZZ_POLY.from_coeffs(vec![Integer::from(-4), Integer::from(0), Integer::from(1)]),
+            ),
+        ] {
+            println!();
+            let rpp = root_prod_poly(&f, &g);
+            println!("f = {}", ZZ_POLY.to_string(&f));
+            println!("g = {}", ZZ_POLY.to_string(&g));
+            println!(
+                "exp = {}    exp_factored = {:?}",
+                ZZ_POLY.to_string(&exp),
+                ZZ_POLY.factorize_by_kroneckers_method(&exp)
+            );
+            println!(
+                "rpp = {}    rpp_factored = {:?}",
+                ZZ_POLY.to_string(&rpp),
+                ZZ_POLY.factorize_by_kroneckers_method(&rpp)
+            );
+            assert!(ZZ_POLY.are_associate(&exp, &rpp));
+        }
     }
 }
