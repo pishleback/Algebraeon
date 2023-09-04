566a3dd397c2abddabbbff80644c057be7e3bb1a
Michael <michael@otopia.co.uk>
2023-09-04 12:26:59 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -36,34 +36,62 @@ fn todo() {
     ]);
     let roots = ZZ_POLY.all_complex_roots(&f);
 
-    let a = QQ_BAR.sum(roots);
+    let a = QQ_BAR.sum(roots.iter().collect());
 
     println!("{:?}", a);
 }
 
 fn main() {
-    let modn = EuclideanQuotient::new_field_unchecked(ZZ, Integer::from(5));
+    // (x+11)*(x-222)*(x-3333)*(x^2+x+1)*(x+67)
 
-    println!("{:?}", modn.all_units());
-
-    let poly_modn = PolynomialRing::new(&modn);
-
-    let f = poly_modn.from_coeffs(vec![Integer::from(-2), Integer::from(3), Integer::from(4)]);
-    let g = poly_modn.from_coeffs(vec![Integer::from(1), Integer::from(4), Integer::from(6)]);
-    let h = poly_modn.from_coeffs(vec![
-        Integer::from(1),
-        Integer::from(4),
-        Integer::from(6),
-        Integer::from(1),
+    let f = ZZ_POLY.product(vec![
+        &ZZ_POLY.from_coeffs(vec![Integer::from(11), Integer::from(1)]),
+        &ZZ_POLY.from_coeffs(vec![Integer::from(-222), Integer::from(1)]),
+        &ZZ_POLY.from_coeffs(vec![Integer::from(-3333), Integer::from(1)]),
+        &ZZ_POLY.from_coeffs(vec![Integer::from(1), Integer::from(1), Integer::from(1)]),
+        &ZZ_POLY.from_coeffs(vec![
+            Integer::from(-1),
+            Integer::from(-1),
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(1),
+        ]),
+        &ZZ_POLY.from_coeffs(vec![Integer::from(67), Integer::from(1)]),
+        &ZZ_POLY.from_coeffs(vec![
+            Integer::from(1),
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(0),
+            Integer::from(1),
+        ]),
     ]);
+    println!("{}", ZZ_POLY.to_string(&f));
 
-    println!("f = {}", poly_modn.to_string(&f));
-    println!("g = {}", poly_modn.to_string(&g));
-    println!("h = {}", poly_modn.to_string(&h));
-
-    let k = poly_modn.product(vec![f, g, h]);
-    println!("k = {}", poly_modn.to_string(&k));
-
-    let fs = poly_modn.factorize_by_trying_all_factors(k);
+    let fs = ZZ_POLY.factorize_by_hensel_lifting(f);
     println!("{:?}", fs);
+
+    // let modn = EuclideanQuotient::new_field_unchecked(ZZ, Integer::from(5));
+
+    // println!("{:?}", modn.all_units());
+
+    // let poly_modn = PolynomialRing::new(&modn);
+
+    // // let f = poly_modn.from_coeffs(vec![
+    // //     Integer::from(-1),
+    // //     Integer::from(1),
+    // //     Integer::from(0),
+    // //     Integer::from(0),
+    // //     Integer::from(0),
+    // //     Integer::from(0),
+    // //     Integer::from(0),
+    // //     Integer::from(0),
+    // //     Integer::from(1),
+    // // ]);
+
+    // // let fs = ZZ_POLY.factor(&f);
+    // // println!("{:?}", fs);
+
+    // let fs = poly_modn.factorize_by_trying_all_factors(f);
+    // println!("{:?}", fs);
 }
