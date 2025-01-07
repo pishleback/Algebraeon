15d7e3b89e5ff0af6947c24899396b2ec647cbb1
Michael <michael@otopia.co.uk>
2025-01-07 22:50:23 +0000

    added katex-header.html to workspace

--- open_docs.py
+++ open_docs.py
@@ -5,3 +5,7 @@ subprocess.run(
     ["cargo", "doc", "--no-deps", "--open", "--document-private-items"], 
     env = env
 )
+
+# The katex-header.html in the workspace folder is used for local builds
+# The katex-header.html in each crate is used for builds on docs.rs
+# It's jank but it works
