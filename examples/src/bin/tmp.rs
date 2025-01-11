c90efd861520950ed81f35d003e32db63d05fd9b
Michael <michael@otopia.co.uk>
2025-01-11 20:17:45 +0000

    Update tmp.rs

--- examples/src/bin/tmp.rs
+++ examples/src/bin/tmp.rs
@@ -5,19 +5,17 @@ use malachite_nz::integer::Integer;
 
 fn main() {
     let x = &Polynomial::<Integer>::var().into_ergonomic();
-    let f = ((x.pow(5) - x + 1) * (x.pow(2) + 1)).into_verbose();
+    let f = (x.pow(5) + x.pow(2) - x + 1).into_verbose();
     // Find the complex roots of f(x)
     for root in f.all_complex_roots() {
         println!("root {} of degree {}", root, root.degree());
     }
     /*
     Output:
+        root ≈-1.328 of degree 3
+        root ≈0.662-0.559i of degree 3
+        root ≈0.662+0.559i of degree 3
         root -i of degree 2
         root i of degree 2
-        root ≈-1.172 of degree 5
-        root ≈-0.179-1.087i of degree 5
-        root ≈-0.179+1.087i of degree 5
-        root ≈0.767-0.352i of degree 5
-        root ≈0.767+0.352i of degree 5
     */
 }
