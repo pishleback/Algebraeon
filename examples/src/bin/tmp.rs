b27ec74ec04202dc42d224976877f247a41a835f
Michael <michael@otopia.co.uk>
2025-01-11 09:19:46 +0000

    Update tmp.rs

--- examples/src/bin/tmp.rs
+++ examples/src/bin/tmp.rs
@@ -2,24 +2,24 @@
 
 
 fn main() {
-    use algebraeon_rings::{linear::matrix::*, number::algebraic::isolated_roots::complex::*};
-    use algebraeon_sets::structure::*;
-    use malachite_q::Rational;
-    // Construct a matrix
-    let a = Matrix::<Rational>::from_rows(vec![
-        vec![5, 4, 2, 1],
-        vec![0, 1, -1, -1],
-        vec![-1, -1, 3, 0],
-        vec![1, 1, -1, 2],
-    ]);
-    // Put it into Jordan Normal Form
-    let j = MatrixStructure::new(ComplexAlgebraic::structure()).jordan_normal_form(&a);
-    j.pprint();
+    use algebraeon_rings::{
+        polynomial::polynomial::*,
+        ring_structure::{elements::*, structure::*},
+    };
+    use malachite_nz::integer::Integer;
+    
+    let x = &Polynomial::<Integer>::var().into_ergonomic();
+    let f = (x.pow(2) - 5*x + 6).into_verbose();
+    println!("f = {}", f.factor().unwrap());
     /*
     Output:
-        / 2    0    0    0 \
-        | 0    1    0    0 |
-        | 0    0    4    1 |
-        \ 0    0    0    4 /
+        f = 1 * ((-2)+λ) * ((-3)+λ)
+    */
+    
+    let f = (x.pow(15) - 1).into_verbose();
+    println!("f = {}", f.factor().unwrap());
+    /*
+    Output:
+        f = 1 * ((-1)+λ) * (1+λ+λ^2) * (1+λ+λ^2+λ^3+λ^4) * (1+(-1)λ+λ^3+(-1)λ^4+λ^5+(-1)λ^7+λ^8)
     */
 }
