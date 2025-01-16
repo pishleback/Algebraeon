39257769925f2759b2e675da3b1a787ff00ca82a
Michael <michael@otopia.co.uk>
2025-01-16 19:52:46 +0000

    re-added p-adic root finding example

--- examples/src/bin/tmp.rs
+++ examples/src/bin/tmp.rs
@@ -10,13 +10,11 @@ use malachite_nz::{integer::Integer, natural::Natural};
 use malachite_q::Rational;
 
 fn main() {
+    use algebraeon_rings::{polynomial::polynomial::*, structure::elements::*};
+    use malachite_nz::{integer::Integer, natural::Natural};
     let x = Polynomial::<Integer>::var().into_ergonomic();
-    let f = (x.pow(6) - 2).into_verbose();
-    println!("f = {}", f);
-    assert_eq!(f.all_padic_roots(&Natural::from(2u32)).len(), 0);
-    assert_eq!(f.all_padic_roots(&Natural::from(7u32)).len(), 0);
-    assert_eq!(f.all_padic_roots(&Natural::from(727u32)).len(), 6);
-    for mut root in f.all_padic_roots(&Natural::from(727u32)) {
-        println!("{}", root);
+    let f = (x.pow(2) - 17).into_verbose();
+    for mut root in f.all_padic_roots(&Natural::from(2u32)) {
+        println!("{}", root.truncate(&20.into()).string_repr()); // Show 20 2-adic digits
     }
 }
