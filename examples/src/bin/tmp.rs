072c1ae6292cd7a895918e0f4d9e80cb967422bf
Michael <michael@otopia.co.uk>
2025-03-28 19:52:43 +0000

    Update tmp.rs

--- examples/src/bin/tmp.rs
+++ examples/src/bin/tmp.rs
@@ -8,7 +8,7 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    let n = Natural::from_str("706000565581575429997696139445280900").unwrap();
+    let n = Natural::from_str("11111111111111111111111111111111111111111111111111111111111111").unwrap();
     let f = factor(n.clone()).unwrap();
     println!("{} = {}", n, f);
 }
