6173461fef9157fcdf437ed30d73c3f89ff61869
pishleback <michael@otopia.co.uk>
2025-04-02 23:16:39 +0100

    Update mod.rs

--- sets/src/combinatorics/mod.rs
+++ sets/src/combinatorics/mod.rs
@@ -19,4 +19,4 @@ pub use set_partitions::Partition;
 pub use subsets::LexicographicSubsetsWithRemovals;
 pub use subsets::subsets;
 pub use subsets::subsets_of_vec;
-pub use twelvefold_way::{FunctionType, TwelvefoldWay};
+// pub use twelvefold_way::{FunctionType, TwelvefoldWay};
