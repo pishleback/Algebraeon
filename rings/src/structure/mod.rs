e6b0f206e3a8f29ce965d1516a12ef2b7c23ced6
Michael <michael@otopia.co.uk>
2025-04-07 23:06:05 +0100

    traits for dedekind domain

--- rings/src/structure/mod.rs
+++ rings/src/structure/mod.rs
@@ -2,8 +2,10 @@ mod elements;
 mod factorization;
 mod quotient;
 mod structure;
+mod ideals;
 
 pub use elements::*;
 pub use factorization::*;
 pub use quotient::*;
 pub use structure::*;
+pub use ideals::*;
