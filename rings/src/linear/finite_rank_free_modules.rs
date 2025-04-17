906364d60096e8f390e5124f03e4127ae247dcad
Michael <michael@otopia.co.uk>
2025-04-17 19:42:06 +0100

    free module on a set

---
+++ rings/src/linear/finite_rank_free_modules.rs
@@ -0,0 +1,5 @@
+// #[derive(Debug, Clone, PartialEq, Eq)]
+// pub struct FreeModuleFiniteNumberedBasisStructure<Ring: RingSignature> {
+//     ring: Ring,
+//     rank: usize,
+// }
--- rings/src/linear/free_modules.rs
+++ rings/src/linear/free_modules.rs
@@ -12,6 +12,25 @@ where
     ring: Ring,
 }
 
+impl<Set: SetSignature, Ring: RingSignature> FreeModuleOverSetStructure<Set, Ring>
+where
+    Set::Set: Eq + Hash,
+{
+    pub fn new(set: Set, ring: Ring) -> Self {
+        Self { set, ring }
+    }
+
+    pub fn reduce(&self, v: <Self as SetSignature>::Set) -> <Self as SetSignature>::Set {
+        v.into_iter()
+            .filter(|(_, r)| !self.ring.is_zero(r))
+            .collect()
+    }
+
+    pub fn basis_element(&self, b: Set::Set) -> <Self as SetSignature>::Set {
+        [(b, self.ring.one())].into()
+    }
+}
+
 impl<Set: SetSignature, Ring: RingSignature> Signature for FreeModuleOverSetStructure<Set, Ring> where
     Set::Set: Eq + Hash
 {
@@ -59,18 +78,20 @@ where
         debug_assert!(self.is_element(v));
         debug_assert!(self.is_element(w));
         let keys = v.keys().chain(w.keys()).unique();
-        keys.map(|k| {
-            (
-                k.clone(),
-                match (v.get(k), w.get(k)) {
-                    (None, None) => unreachable!(),
-                    (None, Some(s)) => s.clone(),
-                    (Some(r), None) => r.clone(),
-                    (Some(r), Some(s)) => self.ring.add(r, s),
-                },
-            )
-        })
-        .collect()
+        self.reduce(
+            keys.map(|k| {
+                (
+                    k.clone(),
+                    match (v.get(k), w.get(k)) {
+                        (None, None) => unreachable!(),
+                        (None, Some(s)) => s.clone(),
+                        (Some(r), None) => r.clone(),
+                        (Some(r), Some(s)) => self.ring.add(r, s),
+                    },
+                )
+            })
+            .collect(),
+        )
     }
 
     fn neg(&self, v: &Self::Set) -> Self::Set {
@@ -82,14 +103,51 @@ where
 
     fn scalar_mul(&self, r: &<Ring>::Set, v: &Self::Set) -> Self::Set {
         debug_assert!(self.is_element(v));
-        v.iter()
-            .map(|(b, s)| (b.clone(), self.ring.mul(r, s)))
-            .collect()
+        if self.ring.is_zero(r) {
+            [].into()
+        } else {
+            v.iter()
+                .map(|(b, s)| (b.clone(), self.ring.mul(r, s)))
+                .collect()
+        }
     }
 }
 
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// pub struct FreeModuleFiniteNumberedBasisStructure<Ring: RingSignature> {
-//     ring: Ring,
-//     rank: usize,
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use algebraeon_nzq::Integer;
+
+    #[test]
+    fn test_finite_rank_modules() {
+        #[derive(Debug, Clone, PartialEq, Eq, Hash, CanonicalStructure)]
+        enum Basis {
+            A,
+            B,
+            C,
+        }
+
+        let m = FreeModuleOverSetStructure::new(Basis::structure(), Integer::structure());
+
+        let a = m.basis_element(Basis::A);
+        let b = m.basis_element(Basis::B);
+        let c = m.basis_element(Basis::C);
+
+        assert_eq!(
+            m.add(&m.neg(&b), &m.add(&a, &b)),
+            [(Basis::A, Integer::ONE)].into()
+        );
+
+        assert_eq!(
+            m.add(&m.add(&a, &b), &m.add(&b, &c)),
+            [
+                (Basis::A, Integer::ONE),
+                (Basis::B, Integer::TWO),
+                (Basis::C, Integer::ONE)
+            ]
+            .into()
+        );
+
+        assert_eq!(m.scalar_mul(&5.into(), &a), [(Basis::A, 5.into())].into());
+    }
+}
