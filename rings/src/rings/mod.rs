e16a762e109aac73131550928976a0099c32cd50
Michael <michael@otopia.co.uk>
2025-04-17 20:50:30 +0100

    Update mod.rs

--- rings/src/rings/mod.rs
+++ rings/src/rings/mod.rs
@@ -1,8 +1,8 @@
-pub mod isolated_algebraic;
 pub mod algebraic_number_fields;
 pub mod finite_fields;
 pub mod integer;
+pub mod isolated_algebraic;
 pub mod natural;
 pub mod num_theory;
-pub mod rational;
 pub mod quotient;
+pub mod rational;
