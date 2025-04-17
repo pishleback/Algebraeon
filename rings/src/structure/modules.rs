e4f32b76530acb538a83c9d51f664ec419e6e24d
Michael <michael@otopia.co.uk>
2025-04-17 19:10:49 +0100

    Update modules.rs

--- rings/src/structure/modules.rs
+++ rings/src/structure/modules.rs
@@ -2,16 +2,16 @@
 use crate::structure::*;
 use algebraeon_sets::structure::*;
 
-pub trait ModuleStructure<Ring: RingSignature>: SetSignature {
-    fn ring(&self) -> Ring;
+pub trait ModuleSignature<Ring: RingSignature>: SetSignature {
+    fn ring(&self) ->&Ring;
     fn add(&self, a: &Self::Set, b: &Self::Set) -> Self::Set;
     fn neg(&self, a: &Self::Set) -> Self::Set;
     fn scalar_mul(&self, x: &Ring::Set, a: &Self::Set) -> Self::Set;
 }
 
-pub trait FreeModuleStructure<Ring: RingSignature>: ModuleStructure<Ring> {}
+pub trait FreeModuleSignature<Ring: RingSignature>: ModuleSignature<Ring> {}
 
-pub trait FiniteRankModuleStructure<Ring: RingSignature>: FreeModuleStructure<Ring> {
+pub trait FiniteRankModuleSignature<Ring: RingSignature>: FreeModuleSignature<Ring> {
     fn basis(&self) -> Vec<Self::Set>;
     fn rank(&self) -> usize {
         self.basis().len()
@@ -20,8 +20,8 @@ pub trait FiniteRankModuleStructure<Ring: RingSignature>: FreeModuleStructure<Ri
 
 pub trait ModuleHomomorphism<
     Ring: RingSignature,
-    Domain: ModuleStructure<Ring>,
-    Range: ModuleStructure<Ring>,
+    Domain: ModuleSignature<Ring>,
+    Range: ModuleSignature<Ring>,
 >: Function<Domain, Range>
 {
 }
