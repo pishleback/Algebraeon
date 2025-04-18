f652fe8da3e0621c6eb97aca77977f11d566bb5c
Michael <michael@otopia.co.uk>
2025-04-18 16:19:54 +0100

    Update mod.rs

--- rings/src/linear/mod.rs
+++ rings/src/linear/mod.rs
@@ -1,6 +1,7 @@
 pub mod finitely_free_modules;
 pub mod finitely_free_submodule;
-pub mod finitely_free_submodule_coset;
+// pub mod finitely_free_submodule_coset; TODO
 pub mod free_modules;
+#[deprecated]
 pub mod linear_subspace;
 pub mod matrix;
