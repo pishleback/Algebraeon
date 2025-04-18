65bd5d48712edfa576162f42f82125e1d3e80055
AnarchistHoneybun <thatonebipanda@gmail.com>
2025-04-18 12:09:23 +0530

    ast+grammar update

--- rings/src/parsing/polynomial.rs
+++ rings/src/parsing/polynomial.rs
@@ -3,74 +3,140 @@ use algebraeon_nzq::*;
 use lalrpop_util::lalrpop_mod;
 use std::collections::{BTreeMap, HashSet};
 use std::str::FromStr;
+use std::fmt;
 
 lalrpop_mod!(polynomial_parser, "/parsing/polynomial_grammar.rs"); // synthesized by LALRPOP
 
-#[test]
-fn tmp_test() {
-    let term_parser = polynomial_parser::TermParser::new();
 
-    println!("{:?}", term_parser.parse("3098473"));
+#[derive(Debug, Clone, PartialEq)]
+pub struct Variable {
+    pub name: String,
 }
 
-// Variable names or numbers
-// e.g. "x" or "123"
-struct Chars {
-    string: String,
+impl fmt::Display for Variable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
 }
 
-// Something in brackets
-// e.g. "(x + y^2 - 2)"
-enum SubExpression {
-    Chars(Chars),
+#[derive(Debug, Clone, PartialEq)]
+pub struct Number {
+    pub value: i64,
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Var(Variable),
+    Num(Number),
     Sum(Sum),
     Product(Product),
     Power(Power),
+    Grouped(Box<Expr>),
+    Neg(Box<Expr>), // Added for unary negation
 }
 
-// A sum
-// e.g. "x + 123 + 4*x^2*y + (x + y^2 - 2)"
-struct Sum {
-    terms: Vec<SumTerm>,
-}
-enum SumTerm {
-    Chars(Chars),
-    Power(Power),
-    Product(Product),
-    SubExpression(SubExpression),
-}
-
-// A product
-// e.g. "4*x^2*y"
-struct Product {
-    terms: Vec<ProductTerm>,
-}
-enum ProductTerm {
-    Chars(Chars),
-    Power(Power),
-    SubExpression(SubExpression),
+impl Expr {
+    pub fn validate_polynomial(&self) -> Result<(), String> {
+        match self {
+            Expr::Power(p) => p.validate_polynomial(),
+            Expr::Product(p) => p.validate_polynomial(),
+            Expr::Sum(s) => s.validate_polynomial(),
+            Expr::Neg(e) => e.validate_polynomial(),
+            Expr::Grouped(e) => e.validate_polynomial(),
+            _ => Ok(()),
+        }
+    }
 }
 
-// A power of something
-// e.g. "x^2" or "(x + y^2)^3"
-struct Power {
-    base: Box<PowerBase>,
-    exponent: Integer,
-}
-enum PowerBase {
-    Chars(Chars),
-    SubExpression(SubExpression),
-}
-
-// Any expression
-enum Expression {
-    Chars(Chars),
-    Sum(Sum),
-    Product(Product),
-    Power(Power),
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Var(v) => write!(f, "{}", v),
+            Self::Num(n) => write!(f, "{}", n),
+            Self::Sum(s) => write!(f, "{}", s),
+            Self::Product(p) => write!(f, "{}", p),
+            Self::Power(p) => write!(f, "{}", p),
+            Self::Grouped(e) => write!(f, "({})", e),
+            Self::Neg(e) => write!(f, "-{}", e),
+        }
+    }
 }
 
-impl Expression {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Sum {
+    pub left: Box<Expr>,
+    pub right: Box<Expr>,
+}
+
+impl Sum {
+    fn validate_polynomial(&self) -> Result<(), String> {
+        self.left.validate_polynomial()?;
+        self.right.validate_polynomial()
+    }
+}
+
+impl fmt::Display for Sum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}", self.left, self.right)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Product {
+    pub left: Box<Expr>,
+    pub right: Box<Expr>,
+}
+
+impl Product {
+    fn validate_polynomial(&self) -> Result<(), String> {
+        if let Expr::Power(p) = self.right.as_ref() {
+            if let Expr::Num(n) = p.exponent.as_ref() {
+                if n.value == -1 {
+                    return Err("Division not allowed in polynomials".to_string());
+                }
+            }
+        }
+        self.left.validate_polynomial()?;
+        self.right.validate_polynomial()
+    }
+}
+
+impl fmt::Display for Product {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} * {}", self.left, self.right)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Power {
+    pub base: Box<Expr>,
+    pub exponent: Box<Expr>,
+}
+
+impl Power {
+    fn validate_polynomial(&self) -> Result<(), String> {
+        if let Expr::Num(n) = self.exponent.as_ref() {
+            if n.value < 0 {
+                return Err("Negative exponents not allowed in polynomials".to_string());
+            }
+        }
+        self.base.validate_polynomial()
+    }
+}
+
+impl fmt::Display for Power {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}^{}", self.base, self.exponent)
+    }
+}
+
+impl Expr {
     fn from_string(input: String) -> Self {
         todo!()
     }
@@ -78,7 +144,7 @@ impl Expression {
     // integer coefficients
     fn to_univariate_integer_polynomial(
         var: char,
-        expression: Expression,
+        expression: Expr,
     ) -> Result<Polynomial<Integer>, String> {
         todo!()
     }
@@ -86,7 +152,7 @@ impl Expression {
     // rational coefficients
     fn to_univariate_rational_polynomial(
         var: char,
-        expression: Expression,
+        expression: Expr,
     ) -> Result<Polynomial<Rational>, String> {
         todo!()
     }
