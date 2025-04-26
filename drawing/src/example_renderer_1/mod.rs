5126fa6aca159ae851f19b54cbfaaea8f4ebb9df
Michael <michael@otopia.co.uk>
2025-04-26 21:53:30 +0100

    remove coordinate in 2d example

--- drawing/src/example_renderer_1/mod.rs
+++ drawing/src/example_renderer_1/mod.rs
@@ -5,13 +5,13 @@ use crate::canvas::{Canvas, RenderTarget, Renderer, RendererInstance, CanvasStat
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
-    position: [f32; 3],
+    position: [f32;2],
     color: [f32; 3],
 }
 
 impl Vertex {
     const ATTRIBS: [wgpu::VertexAttribute; 2] =
-        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+        wgpu::vertex_attr_array![0 => Float32x2, 1 => Float32x3];
 
     fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
@@ -24,23 +24,23 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
+        position: [-0.0868241, 0.49240386],
         color: [0.5, 0.0, 0.5],
     }, // A
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
+        position: [-0.49513406, 0.06958647],
         color: [0.5, 0.5, 0.5],
     }, // B
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
+        position: [-0.21918549, -0.44939706],
         color: [0.5, 0.0, 0.5],
     }, // C
     Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
+        position: [0.35966998, -0.3473291],
         color: [0.5, 0.0, 0.5],
     }, // D
     Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
+        position: [0.44147372, 0.2347359],
         color: [0.5, 0.0, 0.5],
     }, // E
 ];
