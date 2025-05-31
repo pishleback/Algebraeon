a163e2a8d8f9850f7c700579b5301633b8ba384a
Michael <michael@otopia.co.uk>
2025-05-31 22:07:21 +0100

    rm old

--- rings/src/linear/mod.rs
+++ rings/src/linear/mod.rs
@@ -4,8 +4,3 @@ pub mod finitely_free_module;
 pub mod finitely_free_submodule;
 pub mod matrix;
 pub mod ordered_set_free_module;
-
-// pub mod hashable_set_finitely_free_module;
-// pub mod hashable_set_free_module;
-// pub mod set_finitely_free_module;
-// pub mod unstructured_set_free_module;
