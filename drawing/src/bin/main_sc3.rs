4b474417178b5abe411db80ec2724171825186c7
Michael <michael@otopia.co.uk>
2025-08-04 21:59:58 +0100

    Update main_sc3.rs

--- drawing/src/bin/main_sc3.rs
+++ drawing/src/bin/main_sc3.rs
@@ -25,15 +25,6 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    // let space = AffineSpace::new_linear(Rational::structure(), 2);
-    // let p1 = Vector::new( space, vec![Rational::from(0), Rational::from(0)]);
-    // let p2 = Vector::new( space, vec![Rational::from(1), Rational::from(0)]);
-    // let p3 = Vector::new( space, vec![Rational::from(0), Rational::from(1)]);
-
-    // let s1 = Simplex::new( space, vec![p1.clone()]).unwrap();
-    // let s2 = Simplex::new( space, vec![p1.clone(), p2.clone()]).unwrap();
-    // let s3 = Simplex::new( space, vec![p1.clone(), p2.clone(), p3.clone()]).unwrap();
-
     let space = AffineSpace::new_linear(Rational::structure_ref(), 2);
 
     let a = space
@@ -68,24 +59,6 @@ fn main() {
         .to_simplicial_complex()
         .into_forget_labels();
     let x = x.difference(&c);
-
-    // let y = x.clone().refine_to_partial_simplicial_complex().simplify();
-
-    // let mut y = ConvexHull::new_empty( space);
-    // y.extend_by_point(Vector::new(
-    //      space,
-    //     vec![Rational::from(0), Rational::from(0)],
-    // ));
-    // y.extend_by_point(Vector::new(
-    //      space,
-    //     vec![Rational::from(1), Rational::from(0)],
-    // ));
-    // y.extend_by_point(Vector::new(
-    //      space,
-    //     vec![Rational::from(-1), Rational::from(0)],
-    // ));
-    // let y = y.as_simplicial_complex().entire;
-    // let y = y.simplify();
 
     let y = x.closure().difference(&x);
 
