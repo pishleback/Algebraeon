48af1b3725d467ae8895fa2ddabb1dd8579a5dfe
Michael <michael@otopia.co.uk>
2025-09-28 19:34:50 +0100

    Update library_benchmark.rs

--- algebraeon/benches/library_benchmark.rs
+++ algebraeon/benches/library_benchmark.rs
@@ -5,7 +5,7 @@ fn fibonacci(n: u64) -> u64 {
     match n {
         0 => 1,
         1 => 1,
-        2 => 2,
+        // 2 => 2,
         // 3 => 3,
         // 4 => 5,
         n => fibonacci(n - 1) + fibonacci(n - 2),
