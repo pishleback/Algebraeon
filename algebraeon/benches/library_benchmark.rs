ddc3cc9b763a2fbe8ffcf22197ee8674528e6f4a
Michael <michael@otopia.co.uk>
2025-09-28 21:35:34 +0100

    Update library_benchmark.rs

--- algebraeon/benches/library_benchmark.rs
+++ algebraeon/benches/library_benchmark.rs
@@ -7,9 +7,7 @@ use std::hint::black_box;
 use std::str::FromStr;
 
 #[library_benchmark]
-#[bench::zero(Natural::from(0u8))]
-#[bench::one(Natural::from(1u8))]
-#[benches::small(iter = (2u8..=20).map(Natural::from))]
+#[benches::small(iter = (0u8..=20).map(Natural::from))]
 #[bench::large1(Natural::from_str("706000565581575429997696139445280900").unwrap())]
 fn bench_factor_natural(value: Natural) {
     black_box(factor(value));
