353ecf2e4e7cb005bf3dfb288404377f88daed66
Michael <michael@otopia.co.uk>
2025-12-26 11:27:03 +0000

    filled in some impls

--- rings/src/algebraic_number_field/quadratic_number_field.rs
+++ rings/src/algebraic_number_field/quadratic_number_field.rs
@@ -10,7 +10,8 @@ use algebraeon_sets::structure::{
 use crate::algebraic_number_field::quadratic_ring_of_integers::QuadraticRingOfIntegersStructure;
 use crate::algebraic_number_field::structure::AlgebraicIntegerRingInAlgebraicNumberField;
 use crate::structure::{
-    FinitelyGeneratedModuleSignature, FreeModuleSignature, PrincipalRationalSubfieldInclusion,
+    AdditiveMonoidEqSignature, FinitelyGeneratedModuleSignature, FreeModuleSignature,
+    MetaAdditiveMonoidEq, MetaCharZeroRing, PrincipalRationalSubfieldInclusion, RingDivisionError,
     RingHomomorphism, RingHomomorphismRangeModuleStructure, SemiModuleSignature,
 };
 use crate::{
@@ -47,8 +48,20 @@ impl FiniteSetSignature for QuadraticNumberFieldBasisCanonicalStructure {
 
 #[derive(Debug, Clone)]
 pub struct QuadraticNumberFieldElement {
-    integer_part: Rational,
+    /// Represents `rational_part + algebraic_part * sqrt(d)`
+    rational_part: Rational,
     algebraic_part: Rational,
+}
+
+impl QuadraticNumberFieldElement {
+    const ZERO: Self = Self {
+        rational_part: Rational::ZERO,
+        algebraic_part: Rational::ZERO,
+    };
+    const ONE: Self = Self {
+        rational_part: Rational::ONE,
+        algebraic_part: Rational::ZERO,
+    };
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -69,51 +82,70 @@ impl SetSignature for QuadraticNumberFieldStructure {
 
 impl EqSignature for QuadraticNumberFieldStructure {
     fn equal(&self, a: &Self::Set, b: &Self::Set) -> bool {
-        todo!()
+        a.rational_part == b.rational_part && a.algebraic_part == b.algebraic_part
     }
 }
 
 impl AdditiveMonoidSignature for QuadraticNumberFieldStructure {
     fn zero(&self) -> Self::Set {
-        todo!()
+        Self::Set::ZERO
     }
 
     fn add(&self, a: &Self::Set, b: &Self::Set) -> Self::Set {
-        todo!()
+        QuadraticNumberFieldElement {
+            rational_part: &a.rational_part + &b.rational_part,
+            algebraic_part: &a.algebraic_part + &b.algebraic_part,
+        }
     }
 }
 
 impl AdditiveGroupSignature for QuadraticNumberFieldStructure {
     fn neg(&self, a: &Self::Set) -> Self::Set {
-        todo!()
+        QuadraticNumberFieldElement {
+            rational_part: -&a.rational_part,
+            algebraic_part: -&a.algebraic_part,
+        }
     }
 }
 
 impl SemiRingSignature for QuadraticNumberFieldStructure {
     fn one(&self) -> Self::Set {
-        todo!()
+        Self::Set::ONE
     }
 
     fn mul(&self, a: &Self::Set, b: &Self::Set) -> Self::Set {
-        todo!()
+        // (x + y sqrd(d))(z + w sqrt(d)) = (xz + dyw) + (xw + yz) sqrt(d)
+        QuadraticNumberFieldElement {
+            rational_part: &a.rational_part * &b.rational_part
+                + Rational::from(&self.d) * &a.algebraic_part * &b.algebraic_part,
+            algebraic_part: &a.rational_part * &b.algebraic_part
+                + &a.algebraic_part * &b.rational_part,
+        }
     }
 }
 
 impl RingSignature for QuadraticNumberFieldStructure {}
 
 impl SemiRingUnitsSignature for QuadraticNumberFieldStructure {
-    fn inv(&self, a: &Self::Set) -> Result<Self::Set, crate::structure::RingDivisionError> {
-        todo!()
+    fn inv(&self, a: &Self::Set) -> Result<Self::Set, RingDivisionError> {
+        // (x + y sqrt(d))^{-1} = (a - b sqrt(d)) / (x^2 + dy^2)
+        debug_assert!(!self.d.is_zero()); // it's squarefree in particular non-zero
+        let d = &a.rational_part * &a.rational_part
+            + Rational::from(&self.d) * &a.algebraic_part * &a.algebraic_part;
+        debug_assert_eq!(d == Rational::ZERO, self.is_zero(a));
+        if d == Rational::ZERO {
+            return Err(RingDivisionError::DivideByZero);
+        }
+        Ok(QuadraticNumberFieldElement {
+            rational_part: &a.rational_part / &d,
+            algebraic_part: -&a.algebraic_part / d,
+        })
     }
 }
 
 impl IntegralDomainSignature for QuadraticNumberFieldStructure {
-    fn div(
-        &self,
-        a: &Self::Set,
-        b: &Self::Set,
-    ) -> Result<Self::Set, crate::structure::RingDivisionError> {
-        todo!()
+    fn div(&self, a: &Self::Set, b: &Self::Set) -> Result<Self::Set, RingDivisionError> {
+        Ok(self.mul(a, &self.inv(b)?))
     }
 }
 
@@ -125,7 +157,11 @@ impl CharacteristicSignature for QuadraticNumberFieldStructure {
 
 impl CharZeroRingSignature for QuadraticNumberFieldStructure {
     fn try_to_int(&self, x: &Self::Set) -> Option<Integer> {
-        todo!()
+        if x.algebraic_part == Rational::ZERO {
+            x.rational_part.try_to_int()
+        } else {
+            None
+        }
     }
 }
 
@@ -133,7 +169,11 @@ impl FieldSignature for QuadraticNumberFieldStructure {}
 
 impl CharZeroFieldSignature for QuadraticNumberFieldStructure {
     fn try_to_rat(&self, x: &Self::Set) -> Option<Rational> {
-        todo!()
+        if x.algebraic_part == Rational::ZERO {
+            Some(x.rational_part.clone())
+        } else {
+            None
+        }
     }
 }
 
