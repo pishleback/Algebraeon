98792927e751655d0cc74f0750592b6b0eedcd2c
Michael <michael@otopia.co.uk>
2026-01-18 15:09:44 +0000

    store mu instead of neg mu and added a test in 6d

--- rings/src/matrix/lll_reduction.rs
+++ rings/src/matrix/lll_reduction.rs
@@ -25,7 +25,7 @@ impl<
     /// Take the rows of M = `mat` as the basis for a lattice.
     ///
     /// Perform an LLL reduction on the lattice, returning a matrix H such that the rows of H*M are an LLL reduced basis for the lattice.
-    /// 
+    ///
     /// `delta` must be in the range (-0.25, 1] with polynomial time complexity only for `delta` in (-0.25, 1). `delta` = 3/4 is the "default" value to use.
     pub fn lll_row_reduction_algorithm(
         &self,
@@ -48,8 +48,8 @@ impl<
         // variables to hold cached values
         #[derive(Debug)]
         struct CacheEntry<Set> {
-            // for i < k the ith entry is -m_{k,i} = -<bj, bi>/<bi, bi>
-            neg_mu: Vec<Set>,
+            // for i < k the ith entry is m_{k,i} = <bj, bi>/<bi, bi>
+            mu: Vec<Set>,
             // the kth gram-schmidt reduced basis vector bk*
             basis_gs: Vec<Set>,
             // <bk, bk>
@@ -62,26 +62,22 @@ impl<
             let true_mat_gs = self.gram_schmidt_row_orthogonalization(basis.clone(), inner_product);
             for (k, cache_entry) in cache.iter().enumerate() {
                 // check mu
-                assert_eq!(cache_entry.neg_mu.len(), k);
+                assert_eq!(cache_entry.mu.len(), k);
                 for i in 0..k {
                     assert!(
                         self.ring().equal(
-                            &cache_entry.neg_mu[i],
-                            &self.ring().neg(
-                                &self
-                                    .ring()
-                                    .try_divide(
-                                        &inner_product.inner_product(
-                                            &basis.get_row(k),
-                                            &true_mat_gs.get_row(i),
-                                        ),
-                                        &inner_product.inner_product(
-                                            &true_mat_gs.get_row(i),
-                                            &true_mat_gs.get_row(i),
-                                        )
+                            &cache_entry.mu[i],
+                            &self
+                                .ring()
+                                .try_divide(
+                                    &inner_product
+                                        .inner_product(&basis.get_row(k), &true_mat_gs.get_row(i),),
+                                    &inner_product.inner_product(
+                                        &true_mat_gs.get_row(i),
+                                        &true_mat_gs.get_row(i),
                                     )
-                                    .unwrap()
-                            )
+                                )
+                                .unwrap()
                         )
                     );
                 }
@@ -100,7 +96,7 @@ impl<
         };
 
         cache.push(CacheEntry {
-            neg_mu: vec![],
+            mu: vec![],
             basis_gs: basis.get_row(0),
             basis_gs_sq: inner_product.inner_product(&basis.get_row(0), &basis.get_row(0)),
         });
@@ -114,32 +110,28 @@ impl<
             // extend the cache if necessary
             if k >= cache.len() {
                 debug_assert_eq!(k, cache.len());
-                let mut neg_mu_k = Vec::with_capacity(k);
+                let mut mu_k = Vec::with_capacity(k);
                 let mut basis_gs = basis.get_row(k);
                 #[allow(clippy::needless_range_loop)]
                 for i in 0..k {
-                    let neg_mu_k_i = self.ring().neg(
-                        &self
-                            .ring()
-                            .try_divide(
-                                &inner_product.inner_product(&basis.get_row(k), &cache[i].basis_gs),
-                                &cache[i].basis_gs_sq,
-                            )
-                            .unwrap(),
-                    );
-                    vs.add_mut(
-                        &mut basis_gs,
-                        &vs.scalar_mul(&cache[i].basis_gs, &neg_mu_k_i),
-                    );
+                    let mu_k_i = self
+                        .ring()
+                        .try_divide(
+                            &inner_product.inner_product(&basis.get_row(k), &cache[i].basis_gs),
+                            &cache[i].basis_gs_sq,
+                        )
+                        .unwrap();
+
+                    vs.sub_mut(&mut basis_gs, &vs.scalar_mul(&cache[i].basis_gs, &mu_k_i));
                     debug_assert!(
                         self.ring()
                             .is_zero(&inner_product.inner_product(&basis_gs, &cache[i].basis_gs))
                     );
-                    neg_mu_k.push(neg_mu_k_i);
+                    mu_k.push(mu_k_i);
                 }
                 let basis_gs_sq = inner_product.inner_product(&basis_gs, &basis_gs);
                 cache.push(CacheEntry {
-                    neg_mu: neg_mu_k,
+                    mu: mu_k,
                     basis_gs,
                     basis_gs_sq,
                 });
@@ -148,20 +140,10 @@ impl<
             }
 
             for j in 0..k {
-                let neg_mu_rounded = self.ring().round(&cache[k].neg_mu[j]);
-                // b_k += mu_rounded * b_i
-                if neg_mu_rounded != Integer::ZERO {
-                    let neg_mu_rounded = self.ring().from_int(&neg_mu_rounded);
-
-                    // update cache
-                    self.ring()
-                        .sub_mut(&mut cache[k].neg_mu[j], &neg_mu_rounded);
-                    for i in 0..j {
-                        cache[k].neg_mu[i] = self.ring().add(
-                            &cache[k].neg_mu[i],
-                            &self.ring().mul(&neg_mu_rounded, &cache[j].neg_mu[i]),
-                        );
-                    }
+                let mu_rounded = self.ring().round(&cache[k].mu[j]);
+                // b_k -= mu_rounded * b_i
+                if mu_rounded != Integer::ZERO {
+                    let mu_rounded = self.ring().from_int(&mu_rounded);
 
                     // update basis and basis transformation matrix
                     let row_opp = ElementaryOpp::new_row_opp(
@@ -169,12 +151,21 @@ impl<
                         ElementaryOppType::AddRowMul {
                             i: k,
                             j,
-                            x: neg_mu_rounded,
+                            x: self.ring().neg(&mu_rounded),
                         },
                     );
-
+                    // println!("{:?}", row_opp);
                     row_opp.apply(&mut basis);
                     row_opp.apply(&mut h);
+
+                    // update cache
+                    self.ring().sub_mut(&mut cache[k].mu[j], &mu_rounded);
+                    for i in 0..j {
+                        cache[k].mu[i] = self.ring().sub(
+                            &cache[k].mu[i],
+                            &self.ring().mul(&mu_rounded, &cache[j].mu[i]),
+                        );
+                    }
 
                     #[cfg(debug_assertions)]
                     validate_cache(&basis, &cache);
@@ -183,7 +174,7 @@ impl<
 
             // check lovasz condition
             // ||b_k*||^2 >= (delta - mu_{k, k-1}^2) * ||b_{k-1}*||^2
-            let mu_k_km1 = &cache[k].neg_mu[k - 1];
+            let mu_k_km1 = &cache[k].mu[k - 1];
             let bk_sq = &cache[k].basis_gs_sq;
             let bkm1_sq = &cache[k - 1].basis_gs_sq;
 
@@ -210,24 +201,25 @@ impl<
                     self.ring().clone(),
                     ElementaryOppType::Swap(k, k - 1),
                 );
+                // println!("{:?}", row_opp);
                 row_opp.apply(&mut basis);
                 row_opp.apply(&mut h);
 
                 // update cache
                 if k > 1 {
                     for j in 0..(k - 1) {
-                        (cache[k].neg_mu[j], cache[k - 1].neg_mu[j]) =
-                            (cache[k - 1].neg_mu[j].clone(), cache[k].neg_mu[j].clone());
+                        (cache[k].mu[j], cache[k - 1].mu[j]) =
+                            (cache[k - 1].mu[j].clone(), cache[k].mu[j].clone());
                     }
                 }
-                let mu = cache[k].neg_mu[k - 1].clone();
+                let mu = cache[k].mu[k - 1].clone();
                 let basis_gs_sq = self.ring().add(
                     &cache[k].basis_gs_sq,
                     &self
                         .ring()
                         .mul(&self.ring().mul(&mu, &mu), &cache[k - 1].basis_gs_sq),
                 );
-                cache[k].neg_mu[k - 1] = self
+                cache[k].mu[k - 1] = self
                     .ring()
                     .try_divide(
                         &self.ring().mul(&mu, &cache[k - 1].basis_gs_sq),
@@ -235,9 +227,8 @@ impl<
                     )
                     .unwrap();
                 let b = cache[k - 1].basis_gs.clone();
-                cache[k - 1].basis_gs = vs.sub(&cache[k].basis_gs, &vs.scalar_mul(&b, &mu));
-                cache[k].basis_gs = vs.add(
-                    &vs.scalar_mul(&cache[k].basis_gs, &cache[k].neg_mu[k - 1]),
+                cache[k - 1].basis_gs = vs.add(&cache[k].basis_gs, &vs.scalar_mul(&b, &mu));
+                cache[k].basis_gs = vs.sub(
                     &vs.scalar_mul(
                         &b,
                         &self
@@ -245,6 +236,7 @@ impl<
                             .try_divide(&cache[k].basis_gs_sq, &basis_gs_sq)
                             .unwrap(),
                     ),
+                    &vs.scalar_mul(&cache[k].basis_gs, &cache[k].mu[k - 1]),
                 );
                 cache[k].basis_gs_sq = self
                     .ring()
@@ -257,16 +249,13 @@ impl<
                     .unwrap();
                 cache[k - 1].basis_gs_sq = basis_gs_sq;
                 for i in (k + 1)..cache.len() {
-                    let t = cache[i].neg_mu[k].clone();
-                    cache[i].neg_mu[k] = self
+                    let t = cache[i].mu[k].clone();
+                    cache[i].mu[k] = self
                         .ring()
-                        .add(&cache[i].neg_mu[k - 1], &self.ring().mul(&mu, &t));
-                    cache[i].neg_mu[k - 1] = self.ring().sub(
-                        &t,
-                        &self
-                            .ring()
-                            .mul(&cache[k].neg_mu[k - 1], &cache[i].neg_mu[k]),
-                    );
+                        .sub(&cache[i].mu[k - 1], &self.ring().mul(&mu, &t));
+                    cache[i].mu[k - 1] = self
+                        .ring()
+                        .add(&t, &self.ring().mul(&cache[k].mu[k - 1], &cache[i].mu[k]));
                 }
 
                 #[cfg(debug_assertions)]
@@ -327,7 +316,7 @@ mod tests {
     use algebraeon_sets::structure::MetaType;
 
     #[test]
-    fn golden_ratio() {
+    fn approx_golden_ratio() {
         let m = Matrix::<Rational>::from_rows(vec![
             vec![
                 Rational::from_str("1").unwrap(),
@@ -371,6 +360,127 @@ mod tests {
                     Rational::from_str("78").unwrap(),
                     Rational::from_str("-66").unwrap(),
                     Rational::from_str("11").unwrap(),
+                ],
+            ])
+        );
+    }
+
+    #[test]
+    fn approx_degree_5_root() {
+        // Find the polynomial x^5-3x^4-2x^3+x^2+7x-3 of which ~1.1614471390 is a root
+
+        let m = Matrix::<Rational>::from_rows(vec![
+            vec![
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("10000000000").unwrap(),
+            ],
+            vec![
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("11614471390").unwrap(),
+            ],
+            vec![
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("13489594567").unwrap(),
+            ],
+            vec![
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("15667451017").unwrap(),
+            ],
+            vec![
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("18196916159").unwrap(),
+            ],
+            vec![
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("0").unwrap(),
+                Rational::from_str("1").unwrap(),
+                Rational::from_str("21134756212").unwrap(),
+            ],
+        ]);
+
+        m.pprint();
+
+        assert_eq!(
+            m.clone().lll_row_reduction_algorithm(
+                &StandardInnerProduct::new(Rational::structure()),
+                &Rational::from_str("3/4").unwrap(),
+            ),
+            Matrix::<Rational>::from_rows(vec![
+                vec![
+                    Rational::from_str("-3").unwrap(),
+                    Rational::from_str("7").unwrap(),
+                    Rational::from_str("1").unwrap(),
+                    Rational::from_str("-2").unwrap(),
+                    Rational::from_str("-3").unwrap(),
+                    Rational::from_str("1").unwrap(),
+                ],
+                vec![
+                    Rational::from_str("1").unwrap(),
+                    Rational::from_str("-14").unwrap(),
+                    Rational::from_str("45").unwrap(),
+                    Rational::from_str("4").unwrap(),
+                    Rational::from_str("-47").unwrap(),
+                    Rational::from_str("16").unwrap(),
+                ],
+                vec![
+                    Rational::from_str("35").unwrap(),
+                    Rational::from_str("-9").unwrap(),
+                    Rational::from_str("9").unwrap(),
+                    Rational::from_str("-48").unwrap(),
+                    Rational::from_str("-16").unwrap(),
+                    Rational::from_str("32").unwrap(),
+                ],
+                vec![
+                    Rational::from_str("-61").unwrap(),
+                    Rational::from_str("0").unwrap(),
+                    Rational::from_str("21").unwrap(),
+                    Rational::from_str("-25").unwrap(),
+                    Rational::from_str("72").unwrap(),
+                    Rational::from_str("-28").unwrap(),
+                ],
+                vec![
+                    Rational::from_str("-2").unwrap(),
+                    Rational::from_str("56").unwrap(),
+                    Rational::from_str("-43").unwrap(),
+                    Rational::from_str("5").unwrap(),
+                    Rational::from_str("51").unwrap(),
+                    Rational::from_str("-50").unwrap(),
+                ],
+                vec![
+                    Rational::from_str("64").unwrap(),
+                    Rational::from_str("7").unwrap(),
+                    Rational::from_str("15").unwrap(),
+                    Rational::from_str("1").unwrap(),
+                    Rational::from_str("-4").unwrap(),
+                    Rational::from_str("-41").unwrap(),
                 ],
             ])
         );
