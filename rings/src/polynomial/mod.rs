98e728828a4e9631e8d0e5b8a50e060667587de4
Michael <michael@otopia.co.uk>
2026-03-23 23:01:35 +0000

    Update mod.rs

--- rings/src/polynomial/mod.rs
+++ rings/src/polynomial/mod.rs
@@ -1,5 +1,5 @@
 mod factoring;
-mod hensel_lifting;
+mod hensel_lifting_btree;
 mod multipoly;
 mod multipoly_structure;
 mod polynomial_structure;
@@ -7,7 +7,7 @@ mod quotient;
 mod symmetric;
 
 pub use factoring::*;
-pub use hensel_lifting::*;
+pub use hensel_lifting_btree::*;
 pub use multipoly::*;
 pub use multipoly_structure::*;
 pub use polynomial_structure::*;
